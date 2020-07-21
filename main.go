0f04e27d5d05ec1d3fca171389d465c85300f248
peter <folimworld@gmail.com>
2020-07-21 11:17:07 +0900

    test2d

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("hello world0")
+	fmt.Println("hello world1")
+	fmt.Println("hello world2")
+	fmt.Println("hello world3")
 }
